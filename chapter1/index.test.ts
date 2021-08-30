5383c249278924da8c95a9e5801497ec1d6673b3
aShunyaOtuski <shunya.otsuki@access-company.com>
2021-08-30 19:31:10 +0900

    imp: read section2
    - Dollarの副作用

--- chapter1/index.test.ts
+++ chapter1/index.test.ts
@@ -2,6 +2,8 @@ import { Dollar } from './Dollar'
 
 test('mutiplication', () => {
   const five = new Dollar(5)
-  five.times(3)
-  expect(five.amount).toStrictEqual(15)
+  let product: Dollar = five.times(3)
+  expect(product.amount).toStrictEqual(15)
+  product = five.times(4)
+  expect(product.amount).toStrictEqual(20)
 })
